537983861ea36a80986a1931d3487246f57fff84
ZanderKirilov <biross11@gmail.com>
2017-05-09 18:55:10 +0300

    Commit answer on task two

---
+++ 02. Inches to Centimeters/Program.java
@@ -0,0 +1,14 @@
+package p02_InchesToCentimeters;
+	
+	import java.util.Scanner;
+	
+	public class InchesToCentimeters {
+		public static void main(String[] args) {
+			Scanner console = new Scanner(System.in);
+			System.out.println("inches: ");
+			
+			double inches = Double.parseDouble(console.nextLine());
+			double centimeters = inches * 2.54;
+			System.out.println("centimeters = " + centimeters);
+		}
+	}
