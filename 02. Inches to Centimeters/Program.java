9c87936391470f8d810d72e3b3415216c63c2aab
ZanderKirilov <biross11@gmail.com>
2017-05-09 19:08:34 +0300

    Update Program.java

--- 02. Inches to Centimeters/Program.java
+++ 02. Inches to Centimeters/Program.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 public class InchesToCentimeters {
-public static void main(String[] args) {
-	Scanner console = new Scanner(System.in);
-	System.out.println("inches: ");
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("inches: ");
 			
-	double inches = Double.parseDouble(console.nextLine());
-	double centimeters = inches * 2.54;
-	System.out.println("centimeters = " + centimeters);
+		double inches = Double.parseDouble(console.nextLine());
+		double centimeters = inches * 2.54;
+		System.out.println("centimeters = " + centimeters);
 	}
 }
