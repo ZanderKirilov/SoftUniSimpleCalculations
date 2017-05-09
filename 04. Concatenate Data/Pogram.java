f0d06763621febba10334ec561a7d7e4f019952f
ZanderKirilov <biross11@gmail.com>
2017-05-09 18:58:25 +0300

    Commit answer on task four

---
+++ 04. Concatenate Data/Pogram.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  
+		public static void main(String[] args) {
+			Scanner console = new Scanner(System.in);
+			String firstName = console.nextLine();
+			String lastName = console.nextLine();
+			int age = Integer.parseInt(console.nextLine());
+			String town = console.nextLine();
+
+			System.out.printf("You are %s %s, a %d-years old person from %s.", firstName, lastName, age, town);
+		}
+	}
