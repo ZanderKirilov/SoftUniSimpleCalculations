476d172c76fb2723628d3f77af4f4bb008e2ca20
ZanderKirilov <biross11@gmail.com>
2017-05-09 19:10:55 +0300

    Commit answer on task eleven

---
+++ 11. USD to BGN/Program.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class curency {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		double usd = Double.parseDouble(console.nextLine());
+		double bgn = usd * 1.79549;
+
+		System.out.printf("%.2f BGN", bgn);
+	}
+}
